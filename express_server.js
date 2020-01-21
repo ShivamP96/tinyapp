a30600fc4e7b6ffd6410844b2f6b2d501b03c547
Shivam <patelsd96@gmail.com>
2020-01-21 03:48:58 +0000

    added new functionality of post request so that we can take a form submission

--- express_server.js
+++ express_server.js
@@ -9,6 +9,18 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  
+}
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/urls", (request, response) => {
+  console.log(request.body);
+  response.send("Ok");
+});
+
 app.get("/", (request, response) => {
   response.send("Hello!");
 });
@@ -28,6 +40,10 @@ app.get("/urls", (request, response) => {
   response.render("urls_index", templateVars);
 })
 
+app.get("/urls/new", (request, response) => {
+  response.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (request, response) => {
   let templateVars = { shortURL: request.params.shortURL, longURL: urlDatabase[request.params.shortURL]};
   response.render('urls_show', templateVars)
@@ -36,3 +52,4 @@ app.get("/urls/:shortURL", (request, response) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port: ${PORT}!`);
 });
+
